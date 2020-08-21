db528623b4be6151c7880728147cf9dd0a38aa4c
axax4037 <mschen1116@gmail.com>
2020-08-21 15:48:20 +0800

    guess number

---
+++ guess.py
@@ -0,0 +1,17 @@
+import random
+print("猜數字遊戲，請輸入數字範圍 A~B ")
+a = int(input("請輸入起始數字 A : "))
+b = int(input("請輸入結尾數字 B : ")) 
+x = random.randint(a, b)
+print("猜數字遊戲,遊戲開始")
+print(x)
+while True:
+    c = int(input("你猜的數字： "))
+    if x == c:
+        print("恭喜你答對了")
+        print("THE NUMBER IS ", x)
+        break
+    else:
+        if x != c:
+            print("猜錯了，再試一次")
+        
